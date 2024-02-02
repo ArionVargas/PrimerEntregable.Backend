b386a15f300592713663f0bbe1b75ecfff0ac4ed
Arion Vargas <arionvargas07@live.com.ar>
2024-02-02 20:58:02 -0300

    terminando cambios

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
 
 
-import productos from "./productManager.js";
+import productos from "./productManager.js"
 
 let persistirProducto = async () => {
     try {
-        let productosTotales = await productos.getProducts();
+        let productosTotales = await productos.getProducts()
 
         if (productosTotales && Array.isArray(productosTotales)) {
-            console.log(`Productos encontrados en ProductManager: ${productosTotales.length}`);
+            console.log(`Productos encontrados en ProductManager: ${productosTotales.length}`)
         } else {
-            console.log("No se pudieron obtener los productos o la respuesta no es un array.");
+            console.log("No se pudieron obtener los productos o la respuesta no es un array.")
         }
     } catch (error) {
-        console.error("Error al obtener productos:", error);
+        console.error("Error al obtener productos:", error)
     }
 }
 
-persistirProducto();
+persistirProducto()
 
